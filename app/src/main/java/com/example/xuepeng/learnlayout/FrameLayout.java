b7dfb41378de36887ba5d419832b5728cd60fbdf
xuepeng <helloxuepeng@gmail.com>
2018-03-26 17:01:27 +0800

    FrameLayout

---
+++ app/src/main/java/com/example/xuepeng/learnlayout/FrameLayout.java
@@ -0,0 +1,42 @@
+package com.example.xuepeng.learnlayout;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class FrameLayout extends AppCompatActivity  {
+    private android.widget.FrameLayout root;
+    private ImageView tvA,tvB;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        root =findViewById(R.id.root);
+        tvA=findViewById(R.id.ivA);
+        tvB=findViewById(R.id.ivB);
+        showA();
+        root.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tvA.getVisibility()==View.VISIBLE){
+                    showB();
+                }else showA();
+            }
+        });
+
+    }
+    private void showA(){
+        tvA.setVisibility(View.VISIBLE);
+        tvB.setVisibility(View.INVISIBLE);
+    }
+
+    private void showB(){
+        tvB.setVisibility(View.VISIBLE);
+        tvA.setVisibility(View.INVISIBLE);
+    }
+}
+
